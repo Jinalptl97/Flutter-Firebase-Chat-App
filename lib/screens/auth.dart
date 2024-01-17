c523f5afd44cfe263cd2c49474e2ef2e6d66712c
jinal Patel <jinalptl97@gmail.com>
2024-01-17 13:44:30 +0000

    First commit

---
+++ lib/screens/auth.dart
@@ -0,0 +1,166 @@
+import 'package:chat_app/widget/user_image_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+
+final _firebase = FirebaseAuth.instance;
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {
+    return _authScreenState();
+  }
+}
+
+class _authScreenState extends State<AuthScreen> {
+  var _islogin = true;
+  var _enteredemail = '';
+  var _enteredpassword = '';
+  File? _selectedImage;
+  final _form = GlobalKey<FormState>();
+  var _isuploading = false;
+
+  void _submit() async {
+    final _isvalid = _form.currentState!.validate();
+
+    if (!_isvalid || !_islogin && _selectedImage == null) {
+      return;
+    }
+
+    _form.currentState!.save();
+    try {
+      setState(() {
+        _isuploading = true;
+      });
+      if (_islogin) {
+        final userCredential = await _firebase.signInWithEmailAndPassword(
+            email: _enteredemail, password: _enteredpassword);
+        print(userCredential);
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredemail, password: _enteredpassword);
+
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child('${userCredentials.user!.uid}.jpg');
+        await storageRef.putFile(_selectedImage!);
+        final imageurl = await storageRef.getDownloadURL();
+        print(imageurl);
+      }
+    } on FirebaseAuthException catch (error) {
+      if (error.code == 'email-already-in-use') {}
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(error.message ?? 'Authentication Failed'),
+        ),
+      );
+      setState(() {
+        _isuploading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                    top: 30, bottom: 20, left: 20, right: 20),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      key: _form,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          if (!_islogin)
+                            UserImagePicker(
+                              onPickImage: (pickedImage) {
+                                _selectedImage = pickedImage;
+                              },
+                            ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Email Address'),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
+                                return 'Please enter valid email address';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredemail = value!;
+                            },
+                          ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.trim().length < 6) {
+                                return 'Password must be at least 6 characters long';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredpassword = value!;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          if (_isuploading) const CircularProgressIndicator(),
+                          if (!_isuploading)
+                            ElevatedButton(
+                                onPressed: _submit,
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Theme.of(context)
+                                        .colorScheme
+                                        .primaryContainer),
+                                child: Text(_islogin ? 'Login' : 'Sign up')),
+                          if (!_isuploading)
+                            TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _islogin = !_islogin;
+                                  });
+                                },
+                                child: Text(_islogin
+                                    ? 'Create an account'
+                                    : 'I already have an account'))
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
